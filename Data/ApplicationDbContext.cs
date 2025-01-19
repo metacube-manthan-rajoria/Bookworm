5ccf1f7f9d8f4fbf5a206c907876c2421dfca0d0
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2025-01-19 21:10:54 +0530

    Added dbcontext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,11 +1,14 @@
+using Bookworm.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookworm.Data;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
     {
         
     }
+
+    //public DbSet<Category> Category {get; set;}
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 using Bookworm.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
