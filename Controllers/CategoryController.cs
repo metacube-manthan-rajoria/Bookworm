88bc60bcc64d5141809a95ef7f716f077866769c
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2025-01-24 00:40:00 +0530

    Added base add and edit actions

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -7,16 +7,37 @@ namespace Bookworm.Controllers
     public class CategoryController : Controller
     {
         // GET: CategoryController
-        public ActionResult Index()
+        public IActionResult Index()
         {
             List<Category>? categories = ApplicationDbClient.RunSelectQuery();
             ViewBag.categories = categories;
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Add(){
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Category category){
+            return RedirectToAction("Index", "Category");
+        }
+
+        public IActionResult Edit(int id){
+            List<Category>? categories = ApplicationDbClient.RunSelectQuery();
+            Category? editableCategory = null;
+            foreach(var category in categories!){
+                if(category.Id == id) editableCategory = category;
+            }
+
+            ViewBag.category = editableCategory;
+            return View();
+        }
+
         public IActionResult Delete(int id){
             bool deleted = ApplicationDbClient.RunDeleteQuery(id);
-            ViewBag.error = "Could not delete the category";
+            if(!deleted) ViewBag.error = "Could not delete the category";
             return RedirectToAction("Index","Category");
         }
     }
